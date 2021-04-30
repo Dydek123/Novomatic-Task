fb1259f811609404a01e3b73381e296435596b58
Dydek123 <mazurkiewicz707@gmail.com>
2021-04-30 23:44:32 +0200

    Add simple test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import * as actions from './store/actions/tunesActions';
 import * as types from './store/types';
-import App from './App';
+import fetchMock from 'fetch-mock'
 
-describe('')
+describe('actions', () => {
+    afterEach(() => {
+        fetchMock.restore();
+    })
+
+    //Testing fetch API doesn't work
+
+    // it('should set error when empty data', function () {
+    //     fetchMock.getOnce('/search', {
+    //         body: {term:'coldplay', entity:'album'},
+    //         headers: {'content-type': 'application/json'}
+    //     })
+    //     expect(actions.setLoading()).toEqual(expectedAction)
+    // });
+
+    it('should set loading', function () {
+        const expectedAction = {
+            type: types.SET_LOADING,
+
+        }
+        expect(actions.setLoading()).toEqual(expectedAction)
+    });
+
+    it('should set error', function () {
+        const expectedAction = {
+            type: types.SET_ERROR,
+            payload: ''
+
+        }
+        expect(actions.setError()).toEqual(expectedAction)
+    });
+})
